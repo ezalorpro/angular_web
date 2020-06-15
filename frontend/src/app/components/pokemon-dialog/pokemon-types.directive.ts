f5ab82e6badc4feccb003f69f759480bcf34e7e7
kleiver <kleiver615@gmail.com>
2020-06-15 13:54:58 -0400

    directiva types pokemon mejorada

--- frontend/src/app/components/pokemon-dialog/pokemon-types.directive.ts
+++ frontend/src/app/components/pokemon-dialog/pokemon-types.directive.ts
@@ -6,87 +6,19 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class PokemonTypesDirective {
 
   @Input() pokemon_type: string;
+  
+  types: Object = {
+    grass:  '#95e471'  ,  poison:   '#853c9b'  ,  water:  '#4487eb'  ,  fire:     '#eb4f44',
+    flying: '#9daef7'  ,  electric: '#f8eb73'  ,  normal: '#ecece4'  ,  bug:      '#92b378',
+    dark:   '#3d332a'  ,  dragon:   '#825ed6'  ,  fairy:  '#e5b7ee'  ,  fighting: '#a75b3d',
+    ghost:  '#4e3d75'  ,  ground:   '#cec489'  ,  ice:    '#a9eff1'  ,  psychic:  '#e969e2',
+    rock:   '#a09766'  ,  steel:    '#e2e2e2'
+  }
 
-  constructor(
-    private el: ElementRef
-  ) { }
+  constructor(private el: ElementRef) { }
 
   ngOnChanges() {
-
-    console.log(this.pokemon_type)
-
-    if (this.pokemon_type == 'grass') {
-      this.el.nativeElement.style.backgroundColor = '#95e471';
-    }
-
-    if (this.pokemon_type == 'poison') {
-      this.el.nativeElement.style.backgroundColor = '#853c9b';
-    }
-
-    if (this.pokemon_type == 'water') {
-      this.el.nativeElement.style.backgroundColor = '#4487eb';
-    }
-
-    if (this.pokemon_type == 'fire') {
-      this.el.nativeElement.style.backgroundColor = '#eb4f44';
-    }
-
-    if (this.pokemon_type == 'flying') {
-      this.el.nativeElement.style.backgroundColor = '#9daef7';
-    }
-
-    if (this.pokemon_type == 'electric') {
-      this.el.nativeElement.style.backgroundColor = '#f8eb73';
-    }
-
-    if (this.pokemon_type == 'normal') {
-      this.el.nativeElement.style.backgroundColor = '#ecece4';
-    }
-
-    if (this.pokemon_type == 'bug') {
-      this.el.nativeElement.style.backgroundColor = '#92b378';
-    }
-
-    if (this.pokemon_type == 'dark') {
-      this.el.nativeElement.style.backgroundColor = '#3d332a';
-    }
-
-    if (this.pokemon_type == 'dragon') {
-      this.el.nativeElement.style.backgroundColor = '#825ed6';
-    }
-
-    if (this.pokemon_type == 'fairy') {
-      this.el.nativeElement.style.backgroundColor = '#e5b7ee';
-    }
-
-    if (this.pokemon_type == 'fighting') {
-      this.el.nativeElement.style.backgroundColor = '#a75b3d';
-    }
-
-    if (this.pokemon_type == 'ghost') {
-      this.el.nativeElement.style.backgroundColor = '#4e3d75';
-    }
-
-    if (this.pokemon_type == 'ground') {
-      this.el.nativeElement.style.backgroundColor = '#cec489';
-    }
-
-    if (this.pokemon_type == 'ice') {
-      this.el.nativeElement.style.backgroundColor = '#a9eff1';
-    }
-
-    if (this.pokemon_type == 'psychic') {
-      this.el.nativeElement.style.backgroundColor = '#e969e2';
-    }
-
-    if (this.pokemon_type == 'rock') {
-      this.el.nativeElement.style.backgroundColor = '#a09766';
-    }
-
-    if (this.pokemon_type == 'steel') {
-      this.el.nativeElement.style.backgroundColor = '#e2e2e2';
-    }
-
+    this.el.nativeElement.style.backgroundColor = this.types[this.pokemon_type];
   }
 
 }
