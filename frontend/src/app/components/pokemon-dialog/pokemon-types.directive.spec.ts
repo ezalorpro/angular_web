5826c9b7d8e3cc3351e7a1101c256f35eb0caab2
kleiver <kleiver615@gmail.com>
2020-06-22 17:55:56 -0400

    solo backend

--- frontend/src/app/components/pokemon-dialog/pokemon-types.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PokemonTypesDirective } from './pokemon-types.directive';
-
-describe('PokemonTypesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PokemonTypesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
